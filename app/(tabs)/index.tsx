87a94280763c30a01df1f0fa0dd13ce45cdeae18
Hiago R Batista <hiagok.o@gmail.com>
2025-10-22 16:04:16 -0300

    add botao de acao do tutorial e image viewer att icones do app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,20 @@
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import Button from "@/components/Button";
+import ImageViewer from '@/components/ImageViewer';
+
+const PlaceholderImage = require('@/assets/images/placeholder.jpg');
 
 export default function Index() {
   return (
     <View style={styles.container}
     >
-      <Text style={styles.text}>PODCASTs</Text>
-      <Text style={styles.text}>
-        <p>Fabrica de Filmes</p>
-        <iframe data-testid="embed-iframe" style={styles.podcast_box} src="https://open.spotify.com/embed/show/3dAI13iP588c9GT1UoelDB?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-      </Text>
-      <Text style={styles.text}>
-        <p>Frango Fino</p>
-        <iframe data-testid="embed-iframe" style={styles.podcast_box} src="https://open.spotify.com/embed/show/6eMairLQRdKPPKVbcoxSIV?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-      </Text>
-      {/* <Text style={styles.text}>
-        <p>Reinaldo Jaqueline</p>
-        <iframe data-testid="embed-iframe" style={styles.podcast_box} src="https://open.spotify.com/embed/show/2MsTGRXkgN5k0gBBRDV4ok?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-      </Text> */}
-      {/* <Text style={styles.text}>
-        <p>Vortex</p>
-        <iframe data-testid="embed-iframe" style={styles.podcast_box} src="https://open.spotify.com/embed/show/0zLUAET9wfl7hz23eXVtCd?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-      </Text> */}
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -30,17 +24,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  text: {
-    color: '#fff',
-  },
-  podcast_box: {
-    borderRadius: "12px"
+  imageContainer: {
+    flex: 1,
   },
   button: {
     fontSize: 20,
     textDecorationLine: 'underline',
     color: '#fff',
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
---
+++ components/PodcastCarousel.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+
+export default function Podcasts() {
+    return (
+        <View>
+            <Text style={styles.text}>PODCASTs</Text>
+            <Text style={styles.text}>
+                <p>Fabrica de Filmes</p>
+                <iframe data-testid="embed-iframe" style={styles.podcast_box} src="https://open.spotify.com/embed/show/3dAI13iP588c9GT1UoelDB?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </Text>
+            <Text style={styles.text}>
+                <p>Frango Fino</p>
+                <iframe data-testid="embed-iframe" style={styles.podcast_box} src="https://open.spotify.com/embed/show/6eMairLQRdKPPKVbcoxSIV?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </Text>
+            <Text style={styles.text}>
+                <p>Reinaldo Jaqueline</p>
+                <iframe data-testid="embed-iframe" style={styles.podcast_box} src="https://open.spotify.com/embed/show/2MsTGRXkgN5k0gBBRDV4ok?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </Text>
+            <Text style={styles.text}>
+                <p>Vortex</p>
+                <iframe data-testid="embed-iframe" style={styles.podcast_box} src="https://open.spotify.com/embed/show/0zLUAET9wfl7hz23eXVtCd?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#fff',
+  },
+  podcast_box: {
+    borderRadius: "12px"
+  },
+});
